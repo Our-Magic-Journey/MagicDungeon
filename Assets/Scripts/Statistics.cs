e3f374fb1a7f4d0105af218e6761e4bae7f46613
Dominikaninn <dominikprabucki.2002@gmail.com>
2023-09-01 22:04:13 +0200

    Add Statistic
    Add basic statistic

---
+++ Assets/Scripts/Statistics.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Statistics : MonoBehaviour
+{
+
+    public float health;
+    public float speed;
+    public float mana;
+    public float gold;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
