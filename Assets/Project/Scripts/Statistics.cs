da5f2055a654ec4f55a4fa824b3ab301df7d5423
Amon De Shir <amonsir29@gmail.com>
2023-09-01 23:26:35 +0200

    style: fix project structure
    Fix project structure.
    Fix code style of the Statistics.cs file.

---
+++ Assets/Project/Scripts/Statistics.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Statistics : MonoBehaviour {
+    public float health;
+    public float speed;
+    public float mana;
+    public float gold;
+}
